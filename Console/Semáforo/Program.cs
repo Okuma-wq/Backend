97617812fa534f554b46e706a3fede9c82926ee9
pedro Okuma <pedrookuma3@gmail.com>
2020-11-12 23:39:01 -0300

    Projeto Semaforo Realizado

---
+++ Console/Semáforo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Semáforo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite a cor da luz acesa no semaforo: ");
+            string cor = Console.ReadLine();
+
+            string açao = "";
+
+            if (cor == "vermelho") {
+                    açao = "Pare";
+            }
+
+            if (cor == "amarelo") {
+                    açao = "Reduza a velocidade e pare";
+            }
+
+            if (cor == "verde") {
+                    açao = "Continue acelerando ou mantenha a velocidade atual";
+            }
+
+            Console.Write(açao);
+        }
+    }
+}
